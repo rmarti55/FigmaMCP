c9961aab95f2a2e2e2e85e4df67165b6f9658e63
rmarti55 <rmarti55@users.noreply.github.com>
2025-06-09 23:34:41 -0600

    refactor(FilterBar): Make component fully responsive

--- src/stories/FilterBar.tsx
+++ src/stories/FilterBar.tsx
@@ -6,15 +6,16 @@ import { SearchInput } from './SearchInput';
 export function FilterBar() {
   return (
     <div className="p-4 bg-gray-50 border rounded-lg w-full">
-      <div className="flex items-center flex-wrap gap-2 w-full">
+      <div className="flex items-center flex-wrap gap-2">
         <SearchInput />
         <DropdownButton buttonVariant="rectangular" items={['Search Post']} />
         <DropdownButton buttonVariant="rectangular" items={['Show Bookmarked', 'Show All']} />
         <DropdownButton buttonVariant="rectangular" items={['Response', 'No Response']} />
         <DropdownButton buttonVariant="rectangular" items={['Comment Posted Time', 'Last 24 Hours']} />
         <DropdownButton buttonVariant="rectangular" items={['Show Hidden', 'Hide Hidden']} />
-        <div className="flex-auto" />
-        <Button variant="default">REFRESH</Button>
+        <Button variant="default" className="ml-auto">
+          REFRESH
+        </Button>
       </div>
     </div>
   );
